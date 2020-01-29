0468c2f87729381efc0014333f4a82eabfb2f9ac
baptiste <baptiste.heraud@epitech.eu>
2020-01-29 11:41:27 +0100

    Update script

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
 
 echo "Type the type of installation that you want (Digit), followed by [ENTER]:"
+echo "1. install web serveur"
+echo "2. install security"
+echo "3. install mysql"
+echo "4. install all"
 
 update(){
     apt-get update
@@ -12,11 +16,29 @@ base(){
     apt-get -y install wget
     apt-get -y install net-tools
     apt-get -y install curl
+    apt-get -y install python3
     cp script/automate.sh /bin/ && chmod +x /bin/automate.sh
+}
+
+mysql(){
+    apt-get install -y mysql-server
+}
+
+security(){
+   echo "Security"
+   apt-get -y install tcpdump
+   apt-get -y install fail2ban
+}
+
+installAll(){
+   echo "install all"
+   mysql
+   security
 }
 
 read choice
 update
+
 if [ "$choice" -eq "1" ]
 then
     echo "Install web serveur."
@@ -29,12 +51,19 @@ then
     apt-get -y install php7.3
     apt-get -y install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json
     apt-get -y install libapache2-mod-php7.3
+    apt-get -y install certbot python-certbot-apache
 
+    PHPVersion=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3);
+   echo $PHPVersion
 elif [ "$choice" -eq "2" ]
 then
-  echo "Security"
- apt-get -y install tcpdump
- apt-get -y install fail2ban
+  security
+elif [ "$choice" -eq "3" ]
+then
+  mysql
+elif [ "$choice" -eq "4" ]
+then
+  installAll
 else
     echo "Install Base"
     base
