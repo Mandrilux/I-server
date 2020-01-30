8ceecf214a527baa3da1674d38a1cfcad647df2e
baptiste <baptiste.heraud@epitech.eu>
2020-01-30 11:17:03 +0100

    update

--- install.sh
+++ install.sh
@@ -20,6 +20,26 @@ base(){
     cp script/automate.sh /bin/ && chmod +x /bin/automate.sh
 }
 
+webserver(){
+    apache
+}
+
+apache(){
+    echo "Install apache serveur."
+    apt-get -y install apache2
+    apt-get -y install lsb-release apt-transport-https ca-certificates
+    wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /et$
+    update
+    apt-get -y install php7.3
+    apt-get -y install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysq$
+    apt-get -y install libapache2-mod-php7.3
+    apt-get -y install certbot python-certbot-apache
+
+    PHPVersion=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3);
+    echo $PHPVersion
+}
+
 mysql(){
     apt-get install -y mysql-server
 }
@@ -32,8 +52,10 @@ security(){
 
 installAll(){
    echo "install all"
+   base
    mysql
    security
+   web
 }
 
 read choice
@@ -41,20 +63,7 @@ update
 
 if [ "$choice" -eq "1" ]
 then
-    echo "Install web serveur."
-    base
-    apt-get -y install apache2
-    apt-get -y install lsb-release apt-transport-https ca-certificates
-    wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
-    echo "deb https://packages.sury.org/php/ $(lsb_release -sc) main" | tee /etc/apt/sources.list.d/php7.3.list
-    update
-    apt-get -y install php7.3
-    apt-get -y install php7.3-cli php7.3-fpm php7.3-json php7.3-pdo php7.3-mysql php7.3-zip php7.3-gd  php7.3-mbstring php7.3-curl php7.3-xml php7.3-bcmath php7.3-json
-    apt-get -y install libapache2-mod-php7.3
-    apt-get -y install certbot python-certbot-apache
-
-    PHPVersion=$(php --version | head -n 1 | cut -d " " -f 2 | cut -c 1,3);
-   echo $PHPVersion
+    webserver
 elif [ "$choice" -eq "2" ]
 then
   security
@@ -67,5 +76,5 @@ then
 else
     echo "Install Base"
     base
-fi     # $String is null.
+fi
  
